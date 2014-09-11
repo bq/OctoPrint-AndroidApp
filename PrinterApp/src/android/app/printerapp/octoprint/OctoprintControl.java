fcdf5c037f16bd81519a507cfa9a6fa8aa4b9227
Alberto Baeza <alberto.baeza@bq.com>
2014-09-11 17:08:04 +0200

    [CELL-326] Added progress dialog when sending commands to the server

--- PrinterApp/src/android/app/printerapp/octoprint/OctoprintControl.java
+++ PrinterApp/src/android/app/printerapp/octoprint/OctoprintControl.java
@@ -7,10 +7,10 @@ import org.apache.http.entity.StringEntity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ProgressDialog;
 import android.app.printerapp.ItemListActivity;
+import android.app.printerapp.R;
 import android.content.Context;
-import android.util.Log;
-
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 /**
@@ -35,6 +35,10 @@ public class OctoprintControl {
 		} catch (UnsupportedEncodingException e) {	e.printStackTrace();
 		}
 				
+		//Progress dialog to notify command events
+		final ProgressDialog pd = new ProgressDialog(context);
+		pd.setMessage(context.getString(R.string.devices_command_waiting));
+		pd.show();
 
 		HttpClientHandler.post(context,url + HttpUtils.URL_CONTROL, 
 				entity, "application/json", new JsonHttpResponseHandler(){
@@ -44,13 +48,22 @@ public class OctoprintControl {
 					}
 			
 			@Override
+					public void onSuccess(int statusCode, Header[] headers,
+							JSONObject response) {
+						super.onSuccess(statusCode, headers, response);
+						
+						//Dismiss progress dialog
+						pd.dismiss();
+					}
+			
+			@Override
 			public void onFailure(int statusCode, Header[] headers,
 					String responseString, Throwable throwable) {
 
 				super.onFailure(statusCode, headers, responseString, throwable);
 
-				
-				Log.i("OUT",responseString);
+				//Dismiss progress dialog
+				pd.dismiss();
 				ItemListActivity.showDialog(responseString);
 			}
 		});
